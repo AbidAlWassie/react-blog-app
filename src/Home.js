1c0b1859680f49b0587e998f36cf0e1ab78f5df4
Abid Al Wassie <abidalwassie@outlook.com>
2022-01-05 03:07:34 +0600

    updated project

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,23 @@
-const Home = () => {
+import { useState } from "react";
 
-  const handleClick = () => {
-    console.log("clicked");
-  }
+const Home = () => {
+  
+  const [blogs, setBlogs] = useState([
+    { title: 'Web dev tips', body: 'lorem ipsum', author: 'Abid', id: 1 },
+    { title: 'Welcome cooking', body: 'lorem ipsum', author: 'Fahmid', id: 2 },
+    { title: 'My new website', body: 'lorem ipsum', author: 'Dan', id: 3 }
+  ]);
+  
 
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click here</button>
+      {/* <h2>Homepage</h2> */}
+      {blogs.map((blog) => (
+        <div className="blogPreview" key={blog.id}>
+          <h2> {blog.title} </h2>
+          <p>Written by {blog.author} </p>
+        </div>
+      ))}
     </div>
   );
 }
